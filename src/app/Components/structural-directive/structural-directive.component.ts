16b34ffb0b9b3d5cb09a46452ad307f0cf4f828e
Sushil141214 <sushil141214@gmail.com>
2025-03-19 03:46:27 -0700

    I am adding structurel directive component. Implement ngIf and ngFor

---
+++ src/app/Components/structural-directive/structural-directive.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-structural-directive',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './structural-directive.component.html',
+  styleUrl: './structural-directive.component.css'
+})
+export class StructuralDirectiveComponent {
+ isDivVisible:boolean=true;
+ isToggle:boolean=false;
+ isdivCompareNumber:boolean=true;
+ Number1:string='';
+ Number2:string='';
+ selectState:string='';
+ isActive:boolean=false;
+
+ cityArray : string []=['Mumbai','Pune','Gujrat','MP','UP'];
+
+ studentList:any[]=[
+  {studentId:22,Name:'Ramcharan',city:'Mumbai',isActive:false},
+  {studentId:33,Name:'Raju',city:'Deharadun',isActive:false},
+  {studentId:24,Name:'Ranju',city:'Varanshi',isActive:true},
+  {studentId:39,Name:'Rani',city:'Banglor',isActive:false},
+  {studentId:89,Name:'Rohani',city:'Pune',isActive:false}
+] 
+
+
+   divShow(){
+        this.isDivVisible=true;
+          }
+
+  divHide(){
+  this.isDivVisible=false;
+    }
+  divToggle()
+  {
+    this.isToggle=!this.isToggle;
+  }
+
+  divCampare(){
+    if(this.Number1=this.Number2)
+    {
+      this.isdivCompareNumber=true;
+    }
+    else
+    {
+      this.isdivCompareNumber=false;
+
+
+    }
+  }
+
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DataBindingComponent } from './Components/data-binding/data-binding.component';
-import { FormsModule } from '@angular/forms';
+import { StructuralDirectiveComponent } from './Components/structural-directive/structural-directive.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DataBindingComponent,FormsModule],
+  imports: [RouterOutlet,StructuralDirectiveComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
 })
 export class AppComponent {
 
-// Data type of typescript 
-//string,number, boolean, Data
 
-CureseName : string="Angular 18";
-ddlDefault="Maharashtra";
-
-isIndian: boolean=true;
+  
 
 
-txtcolor: string ="bg-primary";
 
 constructor()
 {
-   this.CureseName
-}
-
-showMe(message: string){
-
-  alert(message);
-}
   
+  
+}
+
+
 }
